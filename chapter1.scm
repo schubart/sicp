d468c91ffd88df615228f53d13a56e3fa8620fcb
Michael Schubart <michael@schubart.net>
2009-11-14 19:20:14 +0000

    Exercise 1.16.

--- chapter1.scm
+++ chapter1.scm
@@ -526,8 +526,17 @@
 (check-expt expt-iter)
 
 (define (fast-expt b n)
-  (cond ((= n 0) 1)
+  (cond ((= n 0)   1)
 	((even? n) (square (fast-expt b (/ n 2))))
-	(else (* b (fast-expt b (- n 1))))))
+	(else      (* b (fast-expt b (- n 1))))))
 (check-expt fast-expt)
 
+; Exercise 1.16.
+
+(define (fast-expt-iter b n)
+  (define (iter b n a)
+    (cond ((= n 0)   a)
+	  ((even? n) (iter (square b) (/ n 2) a))
+	  (else      (iter b (- n 1) (* a b)))))
+  (iter b n 1))
+(check-expt fast-expt-iter)
