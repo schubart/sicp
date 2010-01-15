29fdfdc537cc02d8f21eeceb775cc11dcab02702
Michael Schubart <michael@schubart.net>
2010-01-15 22:47:44 +0000

    Exercise 2.26.

--- chapter2.scm
+++ chapter2.scm
@@ -456,3 +456,14 @@
 (assert-= 7
           (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr x)))))))))))))
 
+; Exercise 2.26
+
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+
+(assert-equal? '(1 2 3 4 5 6)
+               (append x y))
+(assert-equal? '((1 2 3) . (4 5 6))
+               (cons x y))
+(assert-equal? '((1 2 3) (4 5 6))
+               (list x y))
